93a1cec8f1bde6c0baffc673f620b1182ede75d4
Mario Herrero <mario.herrero11@educa.madrid.org>
2024-02-09 09:37:01 +0100

    Prueba Gir_1

---
+++ src/Prueba_GitHub/Prueba1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Prueba_GitHub;
+
+/**
+ * 
+ */
+public class Prueba1 {
+
+	/**
+	 * 
+	 */
+	public Prueba1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
